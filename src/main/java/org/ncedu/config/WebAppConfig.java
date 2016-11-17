665591ca6b099976fa63acaecdae0d2fa52f106f
patrickfromspb <paulgargolin@gmail.com>
2016-11-17 23:44:15 +0300

    New changes

--- src/main/java/org/ncedu/config/WebAppConfig.java
+++ src/main/java/org/ncedu/config/WebAppConfig.java
@@ -20,11 +20,59 @@ import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.util.Properties;
 
-@Configuration
-@ComponentScan("org.ncedu")
-@EnableWebMvc
-@EnableTransactionManagement
-public class WebAppConfig extends WebMvcConfigurerAdapter {
+ @Configuration
+ @ComponentScan("org.ncedu")
+ @EnableWebMvc
+ @EnableTransactionManagement
+ @PropertySource("classpath:application.properties")
+ public class WebAppConfig {
+         private static final String PROPERTY_NAME_DATABASE_DRIVER = "db.driver";
+         private static final String PROPERTY_NAME_DATABASE_PASSWORD = "db.password";
+         private static final String PROPERTY_NAME_DATABASE_URL = "db.url";
+         private static final String PROPERTY_NAME_DATABASE_USERNAME = "db.username";
+     
+        private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
+         private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+         private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
+     
+                 @Resource
+         private Environment env;
+     
+                 @Bean
+         public DataSource dataSource() {
+                 DriverManagerDataSource dataSource = new DriverManagerDataSource();
+         
+                         dataSource.setDriverClassName(env.getRequiredProperty(PROPERTY_NAME_DATABASE_DRIVER));
+                 dataSource.setUrl(env.getRequiredProperty(PROPERTY_NAME_DATABASE_URL));
+                 dataSource.setUsername(env.getRequiredProperty(PROPERTY_NAME_DATABASE_USERNAME));
+                 dataSource.setPassword(env.getRequiredProperty(PROPERTY_NAME_DATABASE_PASSWORD));
+         
+                         return dataSource;
+             }
+     
+                 @Bean
+         public LocalSessionFactoryBean sessionFactory() {
+                 LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+                 sessionFactoryBean.setDataSource(dataSource());
+                 sessionFactoryBean.setPackagesToScan(env.getRequiredProperty(PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN));
+                 sessionFactoryBean.setHibernateProperties(hibProperties());
+                 return sessionFactoryBean;
+             }
+     
+                 private Properties hibProperties() {
+                 Properties properties = new Properties();
+                 properties.put(PROPERTY_NAME_HIBERNATE_DIALECT, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
+                 properties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
+                 return properties;
+             }
+     
+                 @Bean
+         public HibernateTransactionManager transactionManager() {
+                 HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+                 transactionManager.setSessionFactory(sessionFactory().getObject());
+                 return transactionManager;
+             }
+     
 
     @Bean
     public UrlBasedViewResolver setupViewResolver() {
@@ -35,8 +83,4 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
         return resolver;
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
-    }
 }
--- src/main/java/org/ncedu/controller/MainController.java
+++ src/main/java/org/ncedu/controller/MainController.java
@@ -3,6 +3,8 @@ package org.ncedu.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  *
  *
@@ -12,7 +14,7 @@ public class MainController {
 
     private static final String OAUTH_URL = "https://oauth.vk.com/authorize";
     private static final String CLIENT_ID = "client_id=5691009";
-    private static final String REDIRECT_URI = "redirect_uri=http://localhost:8080/VKMusicListener/oauth2";
+    private static final String REDIRECT_URI = "redirect_uri=http://localhost:3306/VKMusicListener/oauth2";
     private static final String SCOPE = "scope=65544";
     private static final String RESPONCE_TYPE = "response_type=token&v=5.60";
     private static final String VERSION_API = "v=5.60";
@@ -29,11 +31,16 @@ public class MainController {
                 VERSION_API;
     }
 
+
     @RequestMapping(value = "oauth2")
     public String redirectOverJs() {
         return "auth";
     }
 
+    @RequestMapping(value = "/")
+    public ModelAndView mainPage() {
+        return new ModelAndView("home");
+    }
     @RequestMapping(value = "user")
     public String getUser (@RequestParam(value = "access_token") String token,
                            @RequestParam(value = "user_id") String user_id,
